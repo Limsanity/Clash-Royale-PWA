c04ef27595eaf3f6e1d3919676a431b2e874a105
limsanity <798607646@qq.com>
2019-05-29 22:35:44 +0800

    test workbox

--- src/main.js
+++ src/main.js
@@ -45,9 +45,11 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/serviceWorker.js').then(registration => {
       console.log('SW registered: ', registration)
-      registration.pushManager.subscribe({ userVisibleOnly: true })
     }).catch(registrationError => {
       console.log('SW registration failed: ', registrationError)
+    })
+    navigator.serviceWorker.ready.then((registration) => {
+      registration.pushManager.subscribe({ userVisibleOnly: true })
     })
   })
 }
