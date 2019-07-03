bb0504f771adee123d6ab13363f42167f924646e
limsanity <798607646@qq.com>
2019-07-03 19:07:48 +0800

    优化service worker缓存策略并提高用户体验

---
+++ src/utils/swHelper.js
@@ -0,0 +1,54 @@
+let updateCache = {}
+
+function handleRequest (url, timeout, cacheName) {
+  if (usingCache && updateCache[typeof url === 'string' ? url : url.url]) {
+    return cacheOnly(url)
+  } else {
+    return netWorkFirst(url, timeout, cacheName)
+  }
+}
+
+function netWorkFirst (url, timeout, cacheName) {
+  return caches.open(cacheName).then(async (cache) => {
+    const req = new Promise((resolve, reject) => {
+      fetch(url).then(res => {
+        cache.put(url, res.clone())
+        updateCache[typeof url === 'string' ? url : url.url] = true
+        notify('update')
+        resolve(res)
+      }).catch(() => {
+        reject()
+      })
+    })
+    const timer = new Promise((resolve, reject) => {
+      setTimeout(()=>{
+        reject()
+      }, timeout)
+    })
+    const res = Promise.race([req, timer])
+    return res.then(res => res).catch(() => {
+      return caches.match(url).then((response) => {
+        notify('stale')
+        return response
+      })
+    })
+  })
+}
+
+function cacheOnly (url) {
+  delete updateCache[typeof url === 'string' ? url : url.url]
+  if (Object.keys(updateCache) === 0) {
+    usingCache = false
+  }
+  return caches.match(url)
+}
+
+function notify (msg) {
+  setTimeout(() => {
+    self.clients.matchAll().then(clients => {
+      clients.forEach(client => {
+        client.postMessage(msg)
+      })
+    })
+  }, 3000);
+}
